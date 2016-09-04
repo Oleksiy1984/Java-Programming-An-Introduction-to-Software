8443933be4591980f2f5d0b07a0d2b234a58f18b
Oleksiy1984 <o_orlovskiy@ukr.net>
2016-09-05 00:18:19 +0300

    Update and rename WeatherCSV.java to FindColdestAndLowHumidityDay.java

--- FindColdestAndLowHumidityDay.java
+++ FindColdestAndLowHumidityDay.java
@@ -5,7 +5,7 @@ import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 import java.io.File;
 
-public class WeatherCSV {
+public class FindColdestAndLowHumidityDay {
 
     
     public static void main(String[] args) {
@@ -29,29 +29,59 @@ public class WeatherCSV {
         FileResource fr = new FileResource("weather-2013-01-23.csv");
         CSVParser parser = fr.getCSVParser();
         System.out.print(coldestHourInFile(parser));
-
     }
+    
+    //lowestHumidityInFile()
+     /**************************************
+     * This method returns the CSVRecord that has the lowest humidity
+     *
+     * @param parser
+     */
+    public static CSVRecord lowestHumidityInFile(CSVParser parser) {
+
+        CSVRecord resultRecord = null;
+
+        for (CSVRecord record : parser) {
+
+            if (record.get("Humidity").equals("N/A")) {
+                continue;
+            }
+
+            if (resultRecord == null) {
+                resultRecord = record;
+            } else {
+
+                double humidity = Double.parseDouble(record.get("Humidity"));
+                double lowestHumidity = Double.parseDouble(resultRecord.get("Humidity"));
+                resultRecord = (humidity < lowestHumidity) ? record : resultRecord;
+
+            }
+
+        }//end for File loop;
+
+        return resultRecord;
+    }//end lowestHumidityInFile() method
+
+    //lowestHumidityInManyFiles()
+	/****************************
+	 * This method returns a CSVRecord that has the lowest humidity over all the files. 
+	 * @return 
+	 */
     public static CSVRecord lowestHumidityInManyFiles(){
 
         CSVRecord lowestHumidity = null;
-
         //get the directory
         DirectoryResource dr = new DirectoryResource();
-
         for(File file : dr.selectedFiles()){
-
-
             //get the file
             FileResource fr = new FileResource(file);
-
             //get the CSV praser
-            CSVParser praser = fr.getCSVParser();
-
+            CSVParser parser = fr.getCSVParser();
             //get the lowest Humidity in that document 'file'
-            CSVRecord currLowHumidity = lowestHumidityInFile(praser);
-
+            CSVRecord currLowHumidity = lowestHumidityInFile(parser);
             //compare and assign the CSV record with lower humidity to lowestHumidity;
-            if(lowestHumidity == null || Double.parseDouble(currLowHumidity.get("Humidity")) < Double.parseDouble( lowestHumidity.get("Humidity")))
+            if(lowestHumidity == null || 
+            Double.parseDouble(currLowHumidity.get("Humidity")) < Double.parseDouble( lowestHumidity.get("Humidity")))
                 lowestHumidity = currLowHumidity;
 
         }//end for File loop;
@@ -59,10 +89,12 @@ public class WeatherCSV {
 
         return lowestHumidity;
 
-    }
-    /**
+    }//end lowestHumidityInManyFiles() method
+    
+    //coldestHourInFile()
+    /********************
      * Returns the CSVRecord with the coldest temperature in the file and
-     * thus all the information about the coldest temperature
+     * consequently all the information about the coldest temperature
      *
      * @param parser
      */
@@ -80,13 +112,14 @@ public class WeatherCSV {
                 resultRecord = (temperatureF < coldestTemp) ? record : resultRecord;
             }
 
-        }
+        }//end for CSVRecord loop;
 
         return resultRecord;
 
-    }
+    }// end coldestHourInFile() method
 
-    /**
+// fileWithColdestTemperature()
+    /**************************
      * should return a string that is the name of the file
      * from selected files that has the coldest temperature
      */
@@ -123,38 +156,9 @@ public class WeatherCSV {
         }
 
         return filename;
-    }
+    } // end fileWithColdestTemperature() method
 
-    /**
-     * This method returns the CSVRecord that has the lowest humidity
-     *
-     * @param parser
-     */
-    public static CSVRecord lowestHumidityInFile(CSVParser parser) {
-
-        CSVRecord resultRecord = null;
-
-        for (CSVRecord record : parser) {
-
-            if (record.get("Humidity").equals("N/A")) {
-                continue;
-            }
-
-            if (resultRecord == null) {
-                resultRecord = record;
-            } else {
-
-                double temperatureF = Double.parseDouble(record.get("Humidity"));
-                double coldestTemp = Double.parseDouble(resultRecord.get("Humidity"));
-                resultRecord = (temperatureF < coldestTemp) ? record : resultRecord;
-
-            }
-
-        }
-
-        return resultRecord;
-    }
-
+   // averageTemperatureInFile()
     /**
      * @param parser
      * @return
@@ -166,7 +170,6 @@ public class WeatherCSV {
         int counter = 0;
         for (CSVRecord record : parser) {
 
-
             double recordTemp = Double.parseDouble(record.get("TemperatureF"));
 
             if (recordTemp == -9999) {
@@ -176,15 +179,15 @@ public class WeatherCSV {
                 counter++;
             }
 
-
         }
 
         averageTemp = sum / counter;
 
         return averageTemp;
-    }
+    } // end averageTemperatureInFile() method
 
-    /**
+     //averageTemperatureWithHighHumidityInFile()
+    /*******************************************
      * This method returns a double that represents the average temperature of only
      * those temperatures when the humidity was greater than or equal to value
      *
@@ -212,6 +215,6 @@ public class WeatherCSV {
 
         return averageTemp;
 
-    }
+    } // end averageTemperatureWithHighHumidityInFile() method
 
-}
+}//end
